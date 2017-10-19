1aa271ea5d98c90c1e417035a2fd58e43b6e2d4d
torrent benoit <torrentdu64@gmail.com>
2017-10-19 16:12:13 +0200

    input form create

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -3,21 +3,28 @@ import { reduxForm } from 'redux-form';
 
 
 class Signin extends Component {
+
+  handleFormSubmit({email, password}){
+
+  }
+
+
   render(){
+    const { handleSubmit, fields: {email, password}} = this.props;
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
 
       <fieldset className="form-group">
         <label>Email:</label>
-        <input className="form-control" />
+        <input {...email} className="form-control" />
       </fieldset>
 
       <fieldset className="form-group">
         <label>Password:</label>
-        <input className="form-control" />
+        <input {...password} className="form-control" />
       </fieldset>
 
-      <button className="btn btn-primary">Sign in</button>
+      <button className="btn btn-primary" action="submit" >Sign in</button>
 
       </form>
       );
