f495bc89b96edddb4253a20f829e95c173736af3
torrent benoit <torrentdu64@gmail.com>
2017-10-23 16:11:59 +0200

    route path

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import {browserHistory} from 'react-router';
-import { AUTH_USER, AUTH_ERROR } from './types';
+import { AUTH_USER,UNAUTH_USER, AUTH_ERROR } from './types';
 
 
 
@@ -30,4 +30,9 @@ export function authError(error){
       type: AUTH_ERROR,
       payload: error
     }
+}
+
+export function signoutUser(){
+  localStorage.removeItem('token');
+  return { type: UNAUTH_USER};
 }
