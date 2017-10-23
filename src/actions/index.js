8b2490266a00eee1912c16721627eba8de7e2086
torrent benoit <torrentdu64@gmail.com>
2017-10-23 14:47:57 +0200

    error msg to actions index/type

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import {browserHistory} from 'react-router';
-import { AUTH_USER } from './types';
+import { AUTH_USER, AUTH_ERROR } from './types';
 
 
 
@@ -18,16 +18,16 @@ export function signinUser ({email, password} ){
             browserHistory.push('/feature');
         })
         .catch(() => {
-
+          dispatch(authError('Bad error login incorect'));
         });
-
-
-
-
     };
 
+}
 
 
-
-
+export function authError(error){
+    return {
+      type: AUTH_ERROR,
+      payload: error
+    }
 }
