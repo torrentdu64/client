d8ebff3a65e96bc47bccc90f0f65df42676f01a5
torrent benoit <torrentdu64@gmail.com>
2017-10-24 16:02:24 +0200

    validate trick touched return last function

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -15,6 +15,7 @@ class Signup extends Component {
          <fieldset className="form-group">
           <label>Password:</label>
           <input className="form-control" {...password} type="password" />
+          { password.touched && password.error && <div className="error">{password.error}</div>}
         </fieldset>
          <fieldset className="form-group">
           <label>Confirm Password:</label>
@@ -26,8 +27,17 @@ class Signup extends Component {
   }
 }
 
+function validate(formProps){
+  const errors = {};
+    if(formProps.password !== formProps.passwordConfirm){
+      errors.password = 'password error';
+    }
+  return errors;
+}
+
 
 export default reduxForm({
   form: 'form',
-  fields: ['email', 'password', 'passwordConfirm']
+  fields: ['email', 'password', 'passwordConfirm'],
+  validate
 })(Signup);
