1bf4e8f6b59481c8a7cc68ebe33ab1f0ecf7ef7c
torrent benoit <torrentdu64@gmail.com>
2017-10-25 15:23:04 +0200

    require auth to feature

---
+++ src/components/feature.js
@@ -0,0 +1,17 @@
+import React,{ Component} from 'react';
+
+
+class Feature extends Component {
+  render(){
+      return(
+          <div>feature</div>
+
+
+
+        );
+  }
+}
+
+
+export default Feature;
+
